2f1c5d25e12c47dea500159bdaea7a5e671c2bfa
keven <r.kevensantos7@gmail.com>
2022-06-16 15:22:17 -0400

    alteração na função das  operações

--- func.js
+++ func.js
@@ -4,6 +4,8 @@
   let n1;
   let n2;
 
+  let confirm = true;
+
     let sig = document.getElementsByClassName('sig') 
     let Num = document.getElementsByClassName('num') 
 
@@ -15,7 +17,20 @@
         nuns = Num[n].innerText
         One += nuns
 
+        if(confirm == true){
+          n1 = One;
+        }else{
+          n2 = One;
+        }
+       
+
         ShowNum[0].innerText = One
+
+
+
+        
+        console.log('number 1 =' + n1) 
+        console.log( 'number 2 =' + n2)
       })
     }
 
@@ -23,15 +38,18 @@
 
       let sub;
       sig[s].addEventListener('click', ()=>{
+        confirm = false;
+
         let sg = sig[s].innerText
         Sig = sg;
 
         ShowSig[0].innerText = Sig
 
-        n1 = One;
+        console.log('number 1 =' + n1) 
+        console.log( 'number 2 =' + n2)
+        // n1 = One;
 
         if(One.substring(0) == '-'){
-          One = One
           ShowNum[0].innerText = One          
         }else{
          One = '';
@@ -51,15 +69,56 @@
     let r = document.getElementsByClassName('result') 
   
     r[0].addEventListener('click', ()=>{
+      confirm = true;
         n2 = One;
+
+        // if(n2.substring(0) == '-' + n2){
+        //     console.log('aqui')
+        // } 
+
+        // console.log('-' + n2)
+        // console.log(n2.substring(0))
         
         n1 = parseFloat(n1);
         n2 = parseFloat(n2);
 
-        One = n1 + Sig + n2;
-        One = eval(One);
+        console.log('number 1 = ' + n1) 
+        console.log( 'number 2 = ' + n2)
 
-        console.log(One)
+        switch(Sig){
+
+          case '/':
+            One = n1 / n2;
+            ShowNum[0].innerText = One;
+            console.log('-' + n2)
+            break;
+
+          case '*':
+            One = n1 * n2;
+            ShowNum[0].innerText = One;
+            break;
+
+          case '-':
+            One = n1 - n2;
+            ShowNum[0].innerText = One;
+            break;
+
+          case '+':
+            One = n1 + n2;
+            ShowNum[0].innerText = One;
+            break;
+        }
+
+       
+        // if(One.substring(0) == '-'){
+
+        //   console.log('menos aqui')
+        //   // One = One
+        //   // ShowNum[0].innerText = One          
+        // }else{
+        // //  One = '';
+        // //  ShowNum[0].innerText = One 
+        // }
 
         One = String(One);
         Sig = ''; 
@@ -68,6 +127,8 @@
         ShowSig[0].innerText = Sig
 
     })
+
+
 
     let Ac = document.getElementsByClassName('ac') 
     let del = document.getElementsByClassName('Del') 
